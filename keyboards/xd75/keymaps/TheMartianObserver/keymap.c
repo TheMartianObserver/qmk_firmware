eb9720bcf90d524586cb3d9000b946a8120a791f
Sean Portal <git@f9.io>
2021-05-11 00:03:39 -0400

    Fix the keymap to match what's in the layers.txt file

--- keyboards/xd75/keymaps/TheMartianObserver/keymap.c
+++ keyboards/xd75/keymaps/TheMartianObserver/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_NO, KC_NO,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, KC_NO, KC_NO,
         LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    LGUI_T(KC_F),    KC_G,    KC_HOME, KC_PGUP,   KC_H,    RGUI_T(KC_J),    KC_K,    KC_L,   KC_SCLN,          KC_QUOT, KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN,   KC_N,    KC_M,    KC_COMM, KC_DOT,  LGUI_T(KC_SLSH), KC_UP, RSFT_T(KC_BSLS),
-        OSM(MOD_HYPR), OSM(MOD_MEH), KC_LCTL, KC_LALT, KC_LGUI,  LSFT_T(KC_SPC),  LT(1, KC_DEL), LT(2, KC_BSPC), LT(3, KC_SPC), OSM(MOD_RGUI), OSM(MOD_RALT), OSM(MOD_RCTL), KC_LEFT, KC_DOWN, KC_RGHT
+        OSM(MOD_HYPR), OSM(MOD_MEH), KC_LCTL, KC_LALT, KC_LGUI,  LSFT_T(KC_SPC),  KC_DEL, KC_BSPC, LT(1, KC_SPC), OSM(MOD_RGUI), OSM(MOD_RALT), OSM(MOD_RCTL), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_L1] = LAYOUT_ortho_5x15(

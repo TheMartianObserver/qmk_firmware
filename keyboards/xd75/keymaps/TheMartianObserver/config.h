6d5f9813f095a3836d2eaa132cf1b9ea8b24c3b5
Sean Portal <git@f9.io>
2021-05-13 23:39:16 -0400

    Muck with OSM settings

--- keyboards/xd75/keymaps/TheMartianObserver/config.h
+++ keyboards/xd75/keymaps/TheMartianObserver/config.h
@@ -4,10 +4,13 @@
 #define TAPPING_TERM 200
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 // #define TAPPING_FORCE_HOLD
 
 // Apply the modifier on keys that are tapped during a short hold of a modtap
-#define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD
+
+#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */

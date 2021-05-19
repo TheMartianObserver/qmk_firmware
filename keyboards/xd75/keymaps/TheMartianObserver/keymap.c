f87a6c9f84c26fe5bba1177b7c086f1b5b523045
Sean Portal <git@f9.io>
2021-05-18 21:33:25 -0400

    More modifications

--- keyboards/xd75/keymaps/TheMartianObserver/keymap.c
+++ keyboards/xd75/keymaps/TheMartianObserver/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,               KC_Q,         KC_W,    KC_E,    KC_R,    KC_T, KC_NO, KC_NO,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, KC_GRV, MO(3),
         MT(MOD_LCTL, KC_ESC), KC_A,         KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
         OSM(MOD_LSFT),        KC_Z,         KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN,   KC_N,    KC_M,    KC_COMM, KC_DOT,  LGUI_T(KC_SLSH), KC_UP, RSFT_T(KC_BSLS),
-        OSM(MOD_HYPR),        OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT),OSM(MOD_LGUI), KC_LSFT,  KC_DEL, KC_BSPC, LT(1, KC_SPC), OSL(2), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT),OSM(MOD_LGUI), OSL(1), KC_LSFT,  KC_DEL, KC_BSPC, LT(1, KC_SPC), OSL(2), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_L1] = LAYOUT_ortho_5x15(
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         C(A(KC_DEL)), KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_NO, KC_NO, KC_6,    KC_7,    KC_8,    KC_9,   KC_0, KC_NO, KC_NO,
          KC_CAPS, KC_MINUS, KC_UNDS, KC_EQL, KC_PLUS, KC_LPRN, KC_NO,   KC_NO,   KC_NO, OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_ENT, KC_NO,
          KC_NO, KC_LBRC, KC_RBRC, KC_LCBR,  KC_RCBR, KC_RPRN, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-         _______, _______, _______, _______, _______,_______, KC_NO,   KC_NO,KC_NO, _______, _______, _______, KC_NO, KC_NO, KC_NO
+         _______, _______, _______, _______, _______,_______, _______,   _______,KC_NO, _______, _______, _______, KC_NO, KC_NO, KC_NO
     ),
 
     [_L2] = LAYOUT_ortho_5x15(

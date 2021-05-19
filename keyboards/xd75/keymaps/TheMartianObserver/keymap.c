78f16e339c483c1b6453bac8fff8ffa63dd3346d
Sean Portal <git@f9.io>
2021-05-19 13:35:27 -0400

    More modifications

--- keyboards/xd75/keymaps/TheMartianObserver/keymap.c
+++ keyboards/xd75/keymaps/TheMartianObserver/keymap.c
@@ -88,16 +88,16 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QW] = LAYOUT_ortho_5x15(
-        KC_ESC,               KC_1,         KC_2,    KC_3,    KC_4,    KC_5, KC_NO, C(G(KC_Q)),  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   G(KC_BSPC), KC_BSPC,
-        KC_TAB,               KC_Q,         KC_W,    KC_E,    KC_R,    KC_T, KC_NO, KC_NO,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, KC_GRV, MO(3),
+        KC_ESC,               KC_1,         KC_2,    KC_3,    KC_4,    KC_5, KC_MINUS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   G(KC_BSPC), KC_BSPC,
+        KC_TAB,               KC_Q,         KC_W,    KC_E,    KC_R,    KC_T, KC_UNDS, KC_PLUS,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, KC_GRV, MO(3),
         KC_LCTL, KC_A,         KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
         KC_LGUI,        KC_Z,         KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN,   KC_N,    KC_M,    KC_COMM, KC_DOT,  LGUI_T(KC_SLSH), KC_UP, RSFT_T(KC_BSLS),
         OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OS_CMD, OSL(1), KC_LSFT,  KC_DEL, KC_BSPC, LT(1, KC_SPC), OSL(2), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_L1] = LAYOUT_ortho_5x15(
-        KC_NO,        KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,KC_NO, KC_6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        C(A(KC_DEL)), KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_NO, KC_NO, KC_6,    KC_7,    KC_8,    KC_9,   KC_0, KC_NO, KC_NO,
+        C(A(KC_DEL)),        KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,KC_NO, KC_6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_TAB, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_NO, KC_NO, KC_6,    KC_7,    KC_8,    KC_9,   KC_0, KC_NO, KC_NO,
          KC_CAPS, KC_MINUS, KC_UNDS, KC_EQL, KC_PLUS, KC_LPRN, KC_NO,   KC_NO,   KC_NO, OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_NO, KC_TRNS,
          _______, KC_LBRC, KC_RBRC, KC_LCBR,  KC_RCBR, KC_RPRN, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
          _______, _______, _______, _______, _______,_______, _______,   _______,KC_NO, _______, _______, _______, KC_NO, KC_NO, KC_NO
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_L3] = LAYOUT_ortho_5x15(
-        RESET,  AG_SWAP, AG_NORM, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
+        RESET,  AG_SWAP, AG_NORM, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  C(G(KC_Q)),
         KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO, KC_NO, _______,
          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,KC_NO,
          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -126,6 +126,8 @@ oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_cmd_state = os_up_unqueued;
 
+/* static uint16_t os_cmd_timer; */
+
 void update_oneshot(
     oneshot_state *state,
     uint16_t mod,
@@ -135,6 +137,7 @@ void update_oneshot(
 ) {
     if (keycode == trigger) {
         if (record->event.pressed) {
+          // Disable mod if it is pressed again
           if (*state == os_up_queued) {
             *state = os_up_unqueued;
             unregister_code(mod);
